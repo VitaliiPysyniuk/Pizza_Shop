7f585ab484b0dddb84c45b8314f84669bfb9ae13
Vitaliya_keks <73116690+VitaliiPysyniuk@users.noreply.github.com>
2021-09-20 11:10:04 +0300

    Added new setting to spectacular configuration.

--- configs/spactacular_config.py
+++ configs/spactacular_config.py
@@ -4,5 +4,6 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SCHEMA_PATH_PREFIX': '/api/v1',
     'SERVE_INCLUDE_SCHEMA': False,
-    'SCHEMA_PATH_PREFIX_TRIM': True,
+    # 'SCHEMA_PATH_PREFIX_TRIM': True,
+    'SORT_OPERATIONS': False,
 }
